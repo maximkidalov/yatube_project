cf412b936d03bfb3c4154a814ca236ebc3ee8267
Maxim Kidalov <kidalov2002@gmail.com>
2023-02-08 23:16:51 +0600

    добавлены urls и view-функции

---
+++ yatube/posts/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_posts(request, slug):
+    return HttpResponse('посты, отфильтрованные по группам')
